daab38139f6d25239829d5dba160b4f0903a5560
Bhogendra Kumar Das <bhogendrakumardas123@gmail.com>
2026-06-11 19:26:10 +0545

    add use product code

---
+++ frontend/src/hooks/useProduct.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { getAllProducts } from "../services/productService";
+
+const useProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchProducts = async () => {
+    try {
+      const data = await getAllProducts();
+
+      if (data.success) {
+        setProducts(data.products);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return { products, loading };
+};
+
+export default useProducts;
